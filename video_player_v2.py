26c9f53564d097b5ff2e25b0331ab9755d85ed9d
student <edd1e234@gmail.com>
2020-10-29 16:34:51 -0600

    Should be done. Timer error is still there

--- video_player_v2.py
+++ video_player_v2.py
@@ -3,10 +3,14 @@ import cv2
 import numpy as np
 import base64
 import queue
+import time
 
 print("Running...")
 
 thread_lock = threading.Lock()
+AMOUNT = 10
+FILE_NAME = "clip.mp4"
+MAX_FRAMES = 9999
 
 class imageQueue():
     def __init__(self, amount):
@@ -21,13 +25,14 @@ class imageQueue():
         return queue_empty
 
     def add_image(self, image):
+        # Sub from slots, add to the image
         self.semaphore.acquire()
         thread_lock.acquire()
         self.queue.put(image)
         thread_lock.release()
 
     def get_image(self):
-        # Check before that its empty when retrieving images.
+        # Add to slots, pop image
         self.semaphore.release()
         thread_lock.acquire()
         image = self.queue.get()
@@ -37,20 +42,21 @@ class imageQueue():
 
 
 # Contains color images. 
-image_consumer = imageQueue(24)
-image_producer = imageQueue(24)
+image_consumer = imageQueue(AMOUNT)
+image_producer = imageQueue(AMOUNT)
 
 def extract_frames():
     count = 0
 
-    vid_cap = cv2.VideoCapture("clip.mp4")
+    vid_cap = cv2.VideoCapture(FILE_NAME)
 
     # read first image
     success, image = vid_cap.read()
 
-    while success and count < 9999:
+    while success and count < MAX_FRAMES:
         success, jpg_image = cv2.imencode('.jpg', image)
 
+        # Not needed. 
         jpg_as_text = base64.b64encode(jpg_image)
         
         # Add to queue
@@ -59,17 +65,19 @@ def extract_frames():
         success, image = vid_cap.read()
         count += 1
 
-    print("Finished extracting images")
+    print("Exiting extract thread")
     image_consumer.add_image(None)
 
 
 def convert_images_to_grayscale():
     while True:
+        # Frames have not been loaded 
         if image_consumer.is_empty():
             continue
         
         color_image = image_consumer.get_image()
 
+        # No more frames
         if color_image is None:
             break
 
@@ -77,16 +85,18 @@ def convert_images_to_grayscale():
 
         image_producer.add_image(grayscale_image)
 
-    print("Finished converting images")
+    print("Exiting grayscale thread")
     image_producer.add_image(None)
 
 def display_images():
     while True:
+        # Frames have not been loaded
         if image_producer.is_empty():
             continue
 
         image = image_producer.get_image()
 
+        # No more frames
         if image is None:
             break
 
@@ -94,8 +104,9 @@ def display_images():
         if cv2.waitKey(42) and 0xFF == ord("q"):
             break
 
-    print("Finished displaying images")
+    print("Exiting display thread")
 
+# Threads
 extract_thread = threading.Thread(target=extract_frames)
 grayscale_thread = threading.Thread(target=convert_images_to_grayscale)
 display_thread = threading.Thread(target=display_images)
@@ -103,3 +114,13 @@ display_thread = threading.Thread(target=display_images)
 extract_thread.start()
 grayscale_thread.start()
 display_thread.start()
+
+# Destroy all objects properly, final thread should be the main
+while display_thread.is_alive():
+    time.sleep(1)
+
+print("Exiting main thread")
+
+
+
+
